803365b073aad746d778e7ad785d33b2e3571a90
Ahmed Saidi <saidiahmed2100@gmail.com>
2026-09-05 18:13:28 +0200

    python basics

--- day2.py
+++ day2.py
@@ -1,5 +1,31 @@
 age = input ("how old are you ?")
 
+name = "Ahmed"
+
+job = "Ai enginner"
+
+country = "Tunisia"
+
+weight = 70.5 
+
+is_student = True 
+
 age_next_year = int(age) + 1 
 
 print(f"next year you will be {age_next_year} years old")
+
+
+print(f"my name is {name} I am from {country} and I want to become an {job} in the future ")
+
+print ("les types de données sont : ")
+
+print (f"le type de la variable name est : {type(name)}")
+
+print (f"le type de la variable job est : {type(job)}")
+
+print(f"le type de la variable weight est : {type(weight)}")
+
+print(f"le type de la avariable country est : {type(country)}")
+
+print(f"le type de la variable is_student est :{type(is_student)}")
+
