046e1817c8e5f4f7cb733e543a7fdaa3ebae9976
Ahmed Saidi <saidiahmed2100@gmail.com>
2026-09-05 18:24:49 +0200

    python basics

--- day2.py
+++ day2.py
@@ -1,4 +1,4 @@
-age = input ("how old are you ?")
+age = input ("how old are you ?\n")
 
 name = "Ahmed"
 
@@ -8,9 +8,17 @@ country = "Tunisia"
 
 weight = 70.5 
 
+height = 1.80
+
 is_student = True 
 
 age_next_year = int(age) + 1 
+
+a = 20 
+
+b = 10
+
+print(f"IMC de {name} est : {weight / (height **2)}")
 
 print(f"next year you will be {age_next_year} years old")
 
@@ -28,4 +36,14 @@ print(f"le type de la variable weight est : {type(weight)}")
 print(f"le type de la avariable country est : {type(country)}")
 
 print(f"le type de la variable is_student est :{type(is_student)}")
+
+print(f"l'addition de a et b est : {a+b}")
+
+print(f"la soustraction de a et b est : {a-b}")
+
+print(f"la multiplication de a et b est : {a*b}")
+
+print(f"la division de a et b est : {a/b}")
+
+print(f"le reste de la division de a et b est : {a%b}")
 
